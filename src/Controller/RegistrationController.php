1b782e80a990accec766f9a98f33f5a5b204e7b6
Petko Ichev <gaidinbg@yahoo.com>
2020-01-29 02:40:33 +0200

    twig added

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegistrationController
+class RegistrationController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,20 +23,24 @@ class RegistrationController
         return new Response("first try");
     }
 
-    /**
-     * @Route("/petko/good-look")
-     */
-    public function show()
-    {
-        return new Response("Looking good");
-    }
+//    /**
+//     * @Route("/petko/good-look")
+//     */
+//    public function show()
+//    {
+//        return new Response("Looking good");
+//    }
 
     /**
      * @Route("/petko/{slug}")
      */
     public function fire($slug)
     {
-        return new Response(sprintf("Looking good: %s",$slug ));
+//        return new Response(sprintf("Looking good: %s",$slug ));
+        $amountFields = ['once','pretty darn good','Infinite'];
+        return $this->render('registration/form.html.twig',[
+            'title'=> ucwords(str_replace('-',' ',$slug)),
+            'amount'=> $amountFields,]);
     }
 
 
